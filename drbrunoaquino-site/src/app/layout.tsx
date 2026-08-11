04d1cae85040cad62b9cbcd6cf801bf275be2984
brunoaquino1985 <bruno_aquino_souza@yahoo.com.br>
2026-08-11 20:11:32 -0300

    Implement Facebook Pixel checkout tracking script
    Add Facebook Pixel tracking for checkout initiation.

--- drbrunoaquino-site/src/app/layout.tsx
+++ drbrunoaquino-site/src/app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({
                 __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${pixelId}');fbq('track','PageView');`,
               }}
             />
+            <script dangerouslySetInnerHTML={{ __html: `document.addEventListener('click',function(e){var a=e.target.closest&&e.target.closest('a[href*="pay.hotmart.com"]');if(a&&window.fbq)fbq('track','InitiateCheckout',{value:129.90,currency:'BRL'});},true);` }} />
           </>
         )}
         {gaId && (
